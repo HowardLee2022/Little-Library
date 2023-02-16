df384e4cedfe8787545efd0df1abe92eb34f8409
howard <howardnlee94@gmail.com>
2023-02-16 09:41:04 -0800

    fix

--- public/js/currentbook.js
+++ public/js/currentbook.js
@@ -1,6 +1,5 @@
 document.querySelector("#checkbtn").addEventListener("click",e=>{
     e.preventDefault();
-    console.log("hello")
     
     fetch("/api/book/currentuser/book/remove",{
         method:"PUT"
